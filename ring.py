00309bac11bd9b05a78838b689800afb1f586f48
Benjamin Carlyle <benjamincarlyle@soundadvice.id.au>
2017-01-06 16:48:56 +1000

    Support default/all devices

--- ring.py
+++ ring.py
@@ -3,8 +3,9 @@
 import sys, getopt
 import time
 import pychromecast
+import threading
 
-device='Jason'
+device=None
 mediaformat='video/mp4'
 repeat=1
 
@@ -20,32 +21,59 @@ for o, a in myopts:
 	else:
 		media = a
 
-print device
+devices = None
+if device == None:
+	# All available devices
+	devices = pychromecast.get_chromecasts_as_dict().values()
+else:
+	cast = None;
+	sleep = 0
+	while cast == None:
+		if sleep > 0:
+			time.sleep(sleep)
+			print("Retrying " + device + "...");
+		cast = pychromecast.get_chromecast(friendly_name=device)
+		sleep = 1
+
+lock = threading.Lock();
+
+def playAudio(cast):
+	try:
+		cast.wait()
+		mc = cast.media_controller;
+		mc.stop();
+		lock.acquire();
+		print(cast.device)
+		lock.release();
+		for ii in xrange(repeat):
+			if repeat != 1:
+				lock.acquire();
+				print ii
+				lock.release();
+			for media in args:
+				mc.play_media(media, mediaformat);
+				while mc.status.player_state in [u'UNKNOWN', u'IDLE']:
+					lock.acquire();
+					print(mc.status)
+					lock.release();
+					time.sleep(1)
+				while mc.status.player_state in [u'PLAYING', u'BUFFERING']:
+					lock.acquire();
+					print(mc.status)
+					lock.release();
+					time.sleep(1)
+	except KeyboardInterrupt:
+		thread.interrupt_main();
+
+def runThreads(threads):
+	for thread in threads:
+		thread.start();
+	for thread in threads:
+		thread.join();
+
+print devices
 print mediaformat
 
-cast = None
-sleep = 0
-while cast == None:
-	if sleep > 0:
-		time.sleep(sleep)
-		print("Retrying...");
-	cast = pychromecast.get_chromecast(friendly_name=device)
-	sleep = 1
-cast.wait()
-print(cast.device)
-
-mc = cast.media_controller
-mc.stop();
-
-for ii in xrange(repeat):
-	if repeat != 1:
-		print ii
-	for media in args:
-		mc.play_media(media, mediaformat);
-		while mc.status.player_state in [u'UNKNOWN', u'IDLE']:
-			time.sleep(1)
-		print(mc.status)
-		while mc.status.player_state in [u'PLAYING', u'BUFFERING']:
-			time.sleep(1)
-		print(mc.status)
+threads = map((lambda cast: threading.Thread(target = playAudio, args = [cast])), devices);
+runThreads(threads);
 
